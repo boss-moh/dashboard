95f3ec667a3b00232dbfae21807dc12f233c3acf
boss-moh <moh.saad.abu.kmail@gmail.com>
2025-02-19 13:17:53 +0200

    follow docs

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const dashborad = () => {
+  return <div>dashborad</div>;
+};
+
+export default dashborad;
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,3 +2,9 @@ import { Inter } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
 export const lusitana = inter;
+import { Pacifico } from "next/font/google";
+
+export const pacifico = Pacifico({
+  weight: "400",
+  subsets: ["latin"],
+});
